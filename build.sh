973bdc23df61293a6a1f15adbcfb5120d77bbdee
Michael Murray <ashesfall@almostrealism.com>
2022-04-23 11:32:37 -0700

    fix: improved build.sh and corrected library macro

--- build.sh
+++ build.sh
@@ -7,3 +7,12 @@ cmake -S h5fortran/scripts -B build/hdf5 -DCMAKE_INSTALL_PREFIX=$(pwd)/libs
 cmake -S heclib -B build/heclib
 cmake --build build/hdf5
 cmake --build build/heclib
+
+cp build/heclib/libheclib.a libs/lib/libheclib.a
+
+CC=icc
+CXX=icpc
+F9X=ifort
+
+cmake -S iwfm -B build/iwfm -DHDF5_DIR=$(pwd)/libs
+cmake --build build/iwfm
