5f426f7e0ddc98970fd86ff23cb84224e315ab07
Michael Murray <ashesfall@almostrealism.com>
2022-05-07 19:01:25 -0700

    fix: included steps required for multilayertarget in run.sh

--- run.sh
+++ run.sh
@@ -53,6 +53,12 @@ dos2unix *.dat
 Simulation C2VSimFG.in
 ls -la
 
+cp ../Preprocessor/C2VSimFG_Preprocessor.in ..\\Preprocessor\\C2VSimFG_Preprocessor.in
+cp ../Preprocessor/C2VSimFG_Elements.dat ..\\Preprocessor\\C2VSimFG_Elements.dat
+cp ../Preprocessor/C2VSimFG_Nodes.dat ..\\Preprocessor\\C2VSimFG_Nodes.dat
+cp ../Preprocessor/C2VSimFG_Stratigraphy.dat ..\\Preprocessor\\C2VSimFG_Stratigraphy.dat
+cp ../Preprocessor/C2VSimFG_StreamsSpec.dat ..\\Preprocessor\\C2VSimFG_StreamsSpec.dat
+
 /build/iwfm2obs/iwfm2obs_2017 <iwfm2obs_2015.in
 /build/mlt/MultiLayerTarget MultiLayerTarget.in
 ls -la
