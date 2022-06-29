6ab5571faa5307e3ac9b56ff0d72c0356a861b13
Michael Murray <ashesfall@almostrealism.com>
2022-06-29 12:44:38 -0700

    fix: uploading budget data to S3 for use with athena

--- post/post.sh
+++ post/post.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+printenv
 #python3 /scripts/plot_rz_budget.py /Simulation/..\\Results\\C2VSimFG_RZ_Budget.hdf
 python3 /scripts/budget_etl.py /Simulation/..\\Results\\C2VSimFG_RZ_Budget.hdf
