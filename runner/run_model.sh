930e35be4fe5cb3525c917acac21eee30e2ab354
Michael Murray <ashesfall@almostrealism.com>
2022-07-21 08:48:38 -0700

    feat: run_model.sh now converts hdf to text.

--- runner/run_model.sh
+++ runner/run_model.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 rm /Simulation/GW_MultiLayer.out
 /run_simulation.sh
+
+# Process output
+python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_GW_Budget.hdf
+python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_LWU_Budget.hdf
+python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_RZ_Budget.hdf
