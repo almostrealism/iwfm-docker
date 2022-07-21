8fba634321ec099bd85e65669314e0b857b9ae72
Michael Murray <ashesfall@almostrealism.com>
2022-07-21 08:57:51 -0700

    feat: run hdf to text from the Simulation directory.

--- runner/run_model.sh
+++ runner/run_model.sh
@@ -3,6 +3,7 @@ rm /Simulation/GW_MultiLayer.out
 /run_simulation.sh
 
 # Process output
-python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_GW_Budget.hdf
-python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_LWU_Budget.hdf
-python3 /scripts/budget_hdf_to_text.py /Simulation/..\\Results\\C2VSimFG_RZ_Budget.hdf
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_GW_Budget.hdf
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_LWU_Budget.hdf
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_RZ_Budget.hdf
+cd /
