5c5e1256af0a98934e0d6d59beb0dbe6e1172951
Michael Murray <ashesfall@almostrealism.com>
2022-07-21 12:31:51 -0700

    fix: corrected LWU file name

--- runner/run_model.sh
+++ runner/run_model.sh
@@ -3,7 +3,7 @@ rm /Simulation/GW_MultiLayer.out
 /run_simulation.sh
 
 # Process output
-cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_GW_Budget.hdf
-cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_LWU_Budget.hdf
-cd /Simulation ; python3 /scripts/budget_hdf_to_text.py ..\\Results\\C2VSimFG_RZ_Budget.hdf
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py "..\Results\C2VSimFG_GW_Budget.hdf"
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py "..\Results\C2VSimFG_L&WU_Budget.hdf"
+cd /Simulation ; python3 /scripts/budget_hdf_to_text.py "..\Results\C2VSimFG_RZ_Budget.hdf"
 cd /
