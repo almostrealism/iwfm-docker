cf9241e9d081751220e7e574e28bfe346a9de311
Michael Murray <ashesfall@almostrealism.com>
2022-07-26 13:45:48 -0700

    fix: updated tags in build.sh and build number in CMakeLists.txt

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Building container images..."
-docker build . -t ashesfall/iwfm-base:latest --network=host
-docker build . -f Dockerfile.manager -t ashesfall/iwfm-manager:latest
-docker build . -f Dockerfile.agent -t ashesfall/iwfm-agent:latest
+docker build . -t ashesfall/iwfm-base:1273 --network=host
+docker build . -f Dockerfile.manager -t ashesfall/iwfm-manager:1273
+docker build . -f Dockerfile.agent -t ashesfall/iwfm-agent:1273
 
 echo "Pushing container images..."
-docker push ashesfall/iwfm-base:latest
-docker push ashesfall/iwfm-manager:latest
-docker push ashesfall/iwfm-agent:latest
+docker push ashesfall/iwfm-base:1273
+docker push ashesfall/iwfm-manager:1273
+docker push ashesfall/iwfm-agent:1273
