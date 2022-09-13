0d995e498c9cba412176a5a700c625dc084c34f1
Michael Murray <ashesfall@almostrealism.com>
2022-09-13 11:45:18 -0700

    fix: minor issues with dashboard sync.

--- postprocessing/dashboard_download.py
+++ postprocessing/dashboard_download.py
@@ -5,4 +5,4 @@ dashboard_bucket = os.getenv("DASHBOARDS_BUCKET")
 
 if __name__ == '__main__':
     print("Downloading dashboards from S3...")
-    wr.s3.download(path=f's3://{dashboard_bucket}/dashboards.zip', local_path='/dashboards/dashboards.zip')
+    wr.s3.download(path=f's3://{dashboard_bucket}/dashboards.zip', local_file='/dashboards/dashboards.zip')
