61b861476d90bc9fd1c3df66b63775b8528340a7
Michael Murray <ashesfall@almostrealism.com>
2022-09-26 11:18:53 -0700

    feat: download geojson from the analytics bucket, and deliver it to the dashboard bucket, where it can be picked up by superset.

--- postprocessing/dashboard_upload.py
+++ postprocessing/dashboard_upload.py
@@ -7,3 +7,7 @@ if __name__ == '__main__':
     print("Uploading dashboard data to S3...")
     with open("/dashboards/dashboards.zip", "rb") as local_f:
         wr.s3.upload(local_file=local_f, path=f's3://{dashboard_bucket}/dashboards.zip')
+
+    print("Uploading geojson data to S3...")
+    with open("/dashboards/data.geojson", "rb") as local_f:
+        wr.s3.upload(local_file=local_f, path=f's3://{dashboard_bucket}/data.geojson')
