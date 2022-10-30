bdc2ff7936793245905d69e90516d65c5a70e488
Michael Murray <ashesfall@almostrealism.com>
2022-10-30 14:51:11 -0700

    fix: updated image tags to 1403-php7.4-apache.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 echo "Building container images..."
-docker build . -t ashesfall/iwfm-base:1403-ubuntu20.04 --network=host
-docker build . -f Dockerfile.manager -t ashesfall/iwfm-manager:1403-ubuntu20.04
-docker build . -f Dockerfile.agent -t ashesfall/iwfm-agent:1403-ubuntu20.04
-docker build . -f Dockerfile.pmgr -t ashesfall/iwfm-parallel-mgr:1403-ubuntu20.04
-docker build . -f Dockerfile.pagt -t ashesfall/iwfm-parallel-agt:1403-ubuntu20.04
+docker build . -t ashesfall/iwfm-base:1403-php7.4-apache --network=host
+docker build . -f Dockerfile.manager -t ashesfall/iwfm-manager:1403-php7.4-apache
+docker build . -f Dockerfile.agent -t ashesfall/iwfm-agent:1403-php7.4-apache
+docker build . -f Dockerfile.pmgr -t ashesfall/iwfm-parallel-mgr:1403-php7.4-apache
+docker build . -f Dockerfile.pagt -t ashesfall/iwfm-parallel-agt:1403-php7.4-apache
 
 echo "Pushing container images..."
-docker push ashesfall/iwfm-base:1403-ubuntu20.04
-docker push ashesfall/iwfm-manager:1403-ubuntu20.04
-docker push ashesfall/iwfm-agent:1403-ubuntu20.04
-docker push ashesfall/iwfm-parallel-mgr:1403-ubuntu20.04
-docker push ashesfall/iwfm-parallel-agt:1403-ubuntu20.04
+docker push ashesfall/iwfm-base:1403-php7.4-apache
+docker push ashesfall/iwfm-manager:1403-php7.4-apache
+docker push ashesfall/iwfm-agent:1403-php7.4-apache
+docker push ashesfall/iwfm-parallel-mgr:1403-php7.4-apache
+docker push ashesfall/iwfm-parallel-agt:1403-php7.4-apache
