e00b1eadbe70b0d4c995dcad87b9180faeb6002b
Michael Murray <ashesfall@almostrealism.com>
2022-11-24 10:41:29 -0800

    feat: working towards support for postgres.

--- runner/airflow_manager.sh
+++ runner/airflow_manager.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-airflow db init
-airflow users create \
-    --email admin@example.org --firstname admin \
-    --lastname admin --password $AIRFLOW_PASSWORD \
-    --role Admin --username $AIRFLOW_USERNAME
-airflow scheduler &
-airflow webserver -p 8080
+cat /etc/sudoers
+sudo service postgresql start
+createuser --no-password -s airflow
+
+#airflow db init
+#airflow users create \
+#    --email admin@example.org --firstname admin \
+#    --lastname admin --password $AIRFLOW_PASSWORD \
+#    --role Admin --username $AIRFLOW_USERNAME
+#airflow scheduler &
+#airflow webserver -p 8080
