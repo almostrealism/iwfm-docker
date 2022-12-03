f6de44fcec26e0e0e3ac1e88fc5b759b8c9f1581
Michael Murray <ashesfall@almostrealism.com>
2022-12-03 14:17:35 -0800

    fix: minor changes, enable airflow web.

--- runner/airflow_manager.sh
+++ runner/airflow_manager.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 sudo service postgresql start
 sudo /bin/sh /dbinit.sh
-#psql -U airflow -d airflow -f /postgres_airflow.sql
+# psql -U airflow -d airflow -f /postgres_airflow.sql
 
 airflow db init
 
-#airflow users create \
-#    --email admin@example.org --firstname admin \
-#    --lastname admin --password $AIRFLOW_PASSWORD \
-#    --role Admin --username $AIRFLOW_USERNAME
-#airflow scheduler &
-#airflow webserver -p 8080
+airflow users create \
+    --email admin@example.org --firstname admin \
+    --lastname admin --password $AIRFLOW_PASSWORD \
+    --role Admin --username $AIRFLOW_USERNAME
+airflow scheduler &
+airflow webserver -p 8080
