71e0ff56ac0070367bec19b3503d49e227e55698
Michael Murray <ashesfall@almostrealism.com>
2022-12-08 08:09:08 -0800

    fix: a README for pywfm and some tools for local testing.

---
+++ airflow_agt.sh
@@ -0,0 +1,1 @@
+docker run --network=host ashesfall/iwfm-airflow-agent:latest
---
+++ airflow_mgr.sh
@@ -0,0 +1,1 @@
+docker run -e AIRFLOW_USERNAME='airflow' -e AIRFLOW_PASSWORD='airflow' --network=host ashesfall/iwfm-airflow-manager:latest
