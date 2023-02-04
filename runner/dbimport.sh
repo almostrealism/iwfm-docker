9c666289e6a41cfa1505225039b084c546e81a24
Michael Murray <ashesfall@almostrealism.com>
2023-02-04 11:27:39 -0800

    fix: verbose for pg_restore.

--- runner/dbimport.sh
+++ runner/dbimport.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 runuser -l postgres -c 'dropdb analysis'
-runuser -l postgres -c 'pg_restore -C -d postgres /backups/data.sql'
+runuser -l postgres -c 'pg_restore --verbose -C -d postgres /backups/data.sql'
